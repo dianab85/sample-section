9c3dbfc1b1786d142bfbd184826be45253e83c80
Diana <diana.bergholz@edealer.ca>
2018-10-03 05:33:53 -0400

    update scripts

--- wp-content/themes/ed-template-6-child/library/js/child-home-scripts.js
+++ wp-content/themes/ed-template-6-child/library/js/child-home-scripts.js
@@ -15,9 +15,44 @@
                 $('.custom-banner-sect .custom-banner-sect__img-bg').css('height', 300);     
             }
         }
+
+        function positionBanner(){
+
+            var windowHeight = $(window).height();
+            var windowWidth = $(window).width();
+            var footerHeight = $('.home > footer.wp').height();
+            var bannerHeight = $('.banner-static .banner-border-container').height(); 
+            var remainderSpace = windowHeight - footerHeight - bannerHeight;       
+
+
+            if(windowWidth >= 1030) {                    
+                $('.wp .banner-static').css({'padding-top':'200px','padding-bottom':'80px'});            
+                if(remainderSpace > 280){                    
+                    $('.wp .banner-static').css({'padding-top':remainderSpace*2/3+'px','padding-bottom':remainderSpace/3+'px'});
+                }         
+            }
+        }
+
+        positionBanner();    
         setImgHeight();  
+
+        $( window ).load(function() {        
+            setImgHeight();
+            positionBanner();    
+        });
+
+        $( window ).ready(function() {        
+            setImgHeight();
+            positionBanner();    
+        });
+
         $( window ).resize(function() {        
             setImgHeight();
+            positionBanner();    
         });
+
+
+
+
     }
 }(jQuery) );   
--- wp-content/themes/ed-template-6-child/library/js/child-scripts.js
+++ wp-content/themes/ed-template-6-child/library/js/child-scripts.js
@@ -1,49 +1,102 @@
 ( function ($) {    
 
-    //HD10 - Keep Header On One Line 
-    $(window).load(function () {
-      
-        var parentSelector ='.center-menu nav ul';
-        var childSelector ='.center-menu nav ul li';
-        var dropDownText = 'View Models';
+     //HD10 - Keep Header On One Line
+     var navSelector = 'header nav ul';
+     var dropDownSelector = '.drop-down-menu-list ul';  
 
-        function menuPopup () {
-            var startPx = 768;
-            var windowWidth = $(window).width();
-            var parentWidth = $(parentSelector).outerHeight();
-            var childWidth = $(childSelector).outerHeight();
-            
-            console.log(parentWidth);
-            console.log(childWidth);
+    function compare(nav, dropdown){ 
+        var navNode =  document.body.querySelector(nav);
+        var dropDownNode = document.body.querySelector(dropdown);            
+        var navHeight = navNode.offsetHeight;
+        var navElHeight = navNode.querySelector('li:first-child').offsetHeight;
 
-            if(windowWidth >= startPx && parentWidth > childWidth){
-                console.log('hide menu');
-                //hide menu and create popup
-                $(parentSelector).hide();
-                createDropDown(parentSelector, dropDownText);
-            } else {
-                console.log('show menu');
-                //show menu & destroy popup
-                $(parentSelector).show();
-                removeDropDown(parentSelector);
-            }
-        }
-        menuPopup ();
+        if(navHeight !== navElHeight){
+            dropDownNode.parentNode.parentNode.style.visibility = "visible";
+            navNode.style.visibility = "hidden";                
+        }else{                
+            dropDownNode.parentNode.parentNode.style.visibility = "hidden";
+            navNode.style.visibility = "visible";
+        }          
+    }
 
-        function createDropDown(parent, text){           
-            var dd = "<span class='dd-holder'><span class='dd-button'><span>" + text + "</span><i class='icon-navigation-icons-10'></i></span><div class='dd'><ul>";
-            dd = dd + $(parent).html() + '</ul></div></span>';
-            $(parent).parent().prepend(dd);
-        }
 
-        function removeDropDown(parent){
-            $(parent).parent().find('.dd-holder').remove();            
-        }
-
-        $(window).resize(function () {
-            menuPopup ();      
-        });
+     //set top and bottom padding of search input based on header height 
+     function setSearchBarPadding($element = $("header .secondary-menu .live-search-area")){			
+        var windowWidth = $(window).width();
+        var $parentSelector = $element;				
+        var $formSelector = $parentSelector.find(".search-tool");					
         
-   });
- 
+        $formSelector.css({
+            "padding-top": 0,
+            "padding-bottom": 0
+        });
+
+        var selectorHeight = $formSelector.height();
+        var headerHeight = $("header").height();
+        var padding = (headerHeight - selectorHeight) / 2;
+
+        if (windowWidth < 1010) {		
+            //cover entire header on mobile & Tablet			
+            $formSelector.css({
+                "padding-top": padding + "px",
+                "padding-bottom": padding + "px"
+            });
+        } else {
+            $formSelector.css({
+                "padding-top": 0,
+                "padding-bottom": 0
+            });
+        }			
+    }
+    
+    function openSearchBar() {	
+        $(".search-trigger").click(function(event) {
+            var $parentSelector = $(this)
+            .parents("header")
+            .find(".live-search-area");				
+            $parentSelector.find(".input-search").val("");
+            $parentSelector.toggleClass("active");		
+            setSearchBarPadding($parentSelector);	
+        });    				
+    }
+
+    function closeSearchBar() {
+        //HD10 Search Close
+        $('.close-trigger').click(function (event) {
+            $(this).parents('header').find('.live-search-area').removeClass('active');
+        });	
+        //Click to close Search
+        $('body').bind("click touchstart", function (event) {
+            var clickedElement = event.target;
+            if ($(clickedElement).parents('.live-search-area').length || $(clickedElement).parents('.search-trigger').length) {
+                // do nothing
+            } else if($('.live-search-area').hasClass('active')) {
+                $('.live-search-area').removeClass('active');
+            }
+        });	
+    }
+
+    compare(navSelector, dropDownSelector);
+    openSearchBar();		
+	closeSearchBar();
+
+    $(document).ready(function () {
+        compare(navSelector, dropDownSelector);
+        openSearchBar();		
+        closeSearchBar();
+    });
+
+    $(document).load(function () {
+        compare(navSelector, dropDownSelector);
+        openSearchBar();		
+        closeSearchBar();
+    });
+
+    $(window).resize(function () { 
+        compare(navSelector, dropDownSelector);    
+        if($("header .secondary-menu .live-search-area").hasClass('active')){
+            setSearchBarPadding();
+        }
+    });
+
 }(jQuery) );    
